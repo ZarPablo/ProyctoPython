ee91f08d8fc9287aa5d01b98cafdd21104ff021f
Pablo PC <pablo.albrecht@gmail.com>
2024-06-19 22:55:16 -0300

    4del19del6

--- proyectoPython.py
+++ proyectoPython.py
@@ -1,9 +1,50 @@
+import mysql.connector
+
 class Catalogo:
-    def __init__(self): 
+    def __init__(self, host, user, password, database):
+        
+        self.conn = mysql.connector.connect (
+            host = 'localhost',
+            user = root,
+            password = ZarPablo77,
+            databesa = python_codo
+        )
+        
+        self.cursor = self.conn.cursor(dictionary = True)
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS productos (codigo INT AUTO_INCREMENT PRIMARY KEY,
+                            descripcion VARCHAR(255) NOT NULL,
+                            cantidad INT NOT NULL,
+                            precio DECIMAL(10,2) NOT NULL,
+                            imagen_url VARCHAR(255),
+                            proveedor INT(4)''')
+        self.conn.commit()
+        
+
+    def __init__(self):
         self.productos = []
 
     def agregar_producto(self, codigo, descripcion, cantidad, precio, imagen, proveedor):    
-        if self.consultar_producto(codigo):
+        
+        sql = "INSERT INTO productos (descripcion, cantidad, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s)"
+        valores = (descripcion, cantidad, precio, imagen, proveedor)
+        
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.lastrowid
+    
+    catalogo = Catalogo(host='localhost', user= 'root', password= 'ZarPablo77', database='python_codo')
+    
+    catalogo.agregar_producto('Teclado usb 101 teclas', 10, 4500, 'teclado.jpg', 101)
+    catalogo.agregar_producto('Mouse usb 3 botones', 5, 2500, 'mouse.jpg', 102)
+    catalogo.agregar_producto('Monitor lcd 22"', 15, 52500, 'monitor22p.jpg', 103)
+    catalogo.agregar_producto('Monitor lcd 27"', 25, 78500, 'monitor27p.jpg', 103)
+    catalogo.agregar_producto('Parlante USB', 4, 2500, 'parlante.jpg', 104)
+
+
+
+
+
+'''   if self.consultar_producto(codigo):
             return False
     
         nuevo_producto = {
@@ -90,4 +131,4 @@ catalogo.mostrar_producto(1)
 catalogo.eliminar_producto(1)
 print()
 print("Listado de Productos:")
-catalogo.listar_productos()
+catalogo.listar_productos()  '''
